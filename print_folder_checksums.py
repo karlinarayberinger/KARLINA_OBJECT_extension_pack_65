1a37dcf3885695ef35e0fe2c867008446d235ddb
karbytes <karlina.ray.beringer@protonmail.com>
2026-05-22 06:50:32 -0700

    Update print_folder_checksums.py

--- print_folder_checksums.py
+++ print_folder_checksums.py
@@ -1,7 +1,7 @@
 #########################################################################################
 # file: print_folder_checksums.py
 # type: Python
-# date: 20_APRIL_2026
+# date: 22_MAY_2026
 # author: karbytes
 # license: PUBLIC_DOMAIN 
 #########################################################################################
@@ -65,8 +65,8 @@ def list_file_checksums(folder_path, output_file):
     print("\n\n--------------------------------\n\n")
 
 # Set the folder path to the folder you would like to analyze.
-folder_path = 'karbytes2026_3'  # Replace with your actual folder path.
-output_file = 'karbytes2026_3_checksums.txt'  # Output file (gets overwritten or created new).
+folder_path = 'karbytes2026_4'  # Replace with your actual folder path.
+output_file = 'karbytes2026_4_checksums.txt'  # Output file (gets overwritten or created new).
 
 # Execute the function which is defined in this program file.
 list_file_checksums(folder_path, output_file)
